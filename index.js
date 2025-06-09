04bde5ed10f7b6d2685c4d32dcdc7cd6270fc33e
Gyana prakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-06-09 11:54:03 +0530

    test data commit

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 }
 );
+const testResults = require('./apis/testresult.json');
+app.get('/testresults', (req, res) => {
+  res.json(testResults);
+});
 
 app.listen(5000, () => {
   console.log('Server running on http://localhost:5000');
