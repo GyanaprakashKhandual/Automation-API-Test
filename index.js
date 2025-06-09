9b0c5186366e826b278f298af2838ec2ff16ecf4
Gyana prakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-06-09 12:18:38 +0530

    orgin changes

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const app = express();
 
 
 app.use(cors({
-  origin: ['https://gyanaprakashkhandual.github.io/API_Reports_jewellary/', 'http://127.0.0.1:5500/Repo/API_Reports/index.html'],
+  origin: ['https://gyanaprakashkhandual.github.io', 'http://127.0.0.1:5500'],
   credentials: true
 }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-}
+} 
 );
 const testResults = require('./apis/testresult.json');
 app.get('/testresults', (req, res) => {
